ef1509f4203e7e25d6d3ac46654f2a88233c0574
Marc-André Gill-M <magm102@hotmail.com>
2017-08-21 01:29:48 -0400

    GettingStarted : "If" statements and logical operators

--- SashimiTutorial/INTRO/1-GettingStarted.ps1
+++ SashimiTutorial/INTRO/1-GettingStarted.ps1
@@ -1,9 +1,36 @@
-# Declaring a variable ... 
+#Basic expressions
 
-$myvariable # Creates a variable.
+# 1. "if" statement and logical operators
 
-$myvariable = 42 # Assigns a value to it
+$someBoolean = $true      # Reserved variable name : true
 
-$grass = 32 # Creates a variable and assigns it a value
+if($someBoolean){         # "if" statement
+    $someBoolean = $false # Reserved variable name : false
+}
+else
+{
+    echo "someBoolean is false" 
+}
 
-$grass # Echoes the variable's value
+$someInteger = 45
+
+if($someInteger -eq 45)  # logical operator "equal to"
+<# 
+An arbitrary amount of "blank space" characters 
+can be put between ')' and '{', as well as comments
+#>
+
+
+{
+    $someInteger = 0
+}
+elseif($someInteger -gt 23){ # elseif block : "else" and "if" identifiers must be bound together
+    echo "someInteger is not equal to 45 and is greater than 23"
+}
+
+if(($someInteger -lt 41) -and $someBoolean) #chained logical expressions : priority established by parenthesis
+{
+    echo "someInteger is lesser than 41 and someBoolean is true"
+}
+
+#See a list of all logical operators and their syntax here : https://ss64.com/ps/syntax-compare.html
